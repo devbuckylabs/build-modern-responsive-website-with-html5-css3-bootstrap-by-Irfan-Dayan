5b108f9ee102548e549d0a942836dc2b34f4a0a5
devbuckylabs <45965699+devbuckylabs@users.noreply.github.com>
2019-12-16 16:12:33 +0530

    12. Building Testimonials Section ----> Completed

---
+++ Vesco/js/custom.js
@@ -0,0 +1,48 @@
+$(function () {
+
+    new WOW().init();
+
+});
+
+$(function () {
+
+    $('#work').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            // options for gallery
+            enabled: true
+
+        }
+
+    });
+
+
+});
+
+$(function () {
+
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+
+    });
+
+});
+
+
+$(function () {
+
+    $("#customers-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+
+    });
+
+});
